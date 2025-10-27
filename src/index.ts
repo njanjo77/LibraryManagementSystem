ee798e055a8e2654bb77e1665858affde1b3f3ea
njanjo77 <njanjovictor@gmail.com>
2025-10-27 15:49:13 -0600

    my branch

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,45 @@ const port = 8081
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
+app.get("/users", (req, res) => {
+    getpool().then(pool => {
+        return pool.request()
+            .query('SELECT * FROM Users')
+    }).then(result => {
+        console.log("Users fetched successfully");
+        res.json(result.recordset)
+    }).catch((err: any) => {
+        console.error("Error fetching users", err)
+        res.status(500).send("Internal Server Error")
+    })
+})
+
+app.get("/books", (req, res) => {
+    getpool().then(pool => {
+        return pool.request()
+            .query('SELECT * FROM Books')
+    }).then(result => {
+        console.log("Books fetched successfully");
+        res.json(result.recordset)
+    }).catch((err: any) => {
+        console.error("Error fetching books", err)
+        res.status(500).send("Internal Server Error")
+    })
+})
+
+app.get("/categories", (req, res) => {
+    getpool().then(pool => {
+        return pool.request()
+            .query('SELECT * FROM Categories')
+    }).then(result => {
+        console.log("Categories fetched successfully");
+        res.json(result.recordset)
+    }).catch((err: any) => {
+        console.error("Error fetching categories", err)
+        res.status(500).send("Internal Server Error")
+    })
+})
+
 app.get("/borrowrecords", (req, res) => {
     getpool().then(pool => {
         return pool.request()
@@ -21,6 +60,19 @@ app.get("/borrowrecords", (req, res) => {
         res.json(result.recordset)
     }).catch((err: any) => {
         console.error("Error fetching borrow records", err)
+        res.status(500).send("Internal Server Error")
+    })
+})
+
+app.get("/comments", (req, res) => {
+    getpool().then(pool => {
+        return pool.request()
+            .query('SELECT * FROM Comments')
+    }).then(result => {
+        console.log("Comments fetched successfully");
+        res.json(result.recordset)
+    }).catch((err: any) => {
+        console.error("Error fetching comments", err)
         res.status(500).send("Internal Server Error")
     })
 })
