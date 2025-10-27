f0fde595cc9c3ce1d2a14efe44fd745df624c8a5
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-10-27 16:57:09 +0300

    configured db connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 
 import  express from "express";
-import { getPool } from './config/database';
-
+import { config } from "dotenv";
+import { Request,Response } from "express";
+import { getPool } from "./config/database.js";
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hello, the express server is running")
@@ -12,6 +14,13 @@ const port = 8081
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
+
+await getPool().then(()=>{console.log("Connected to db Successfully")})
+.catch((err)=>{console.log("Failed to connect to db.")})
+
+
+
+
 
 
 
