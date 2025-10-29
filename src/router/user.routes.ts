288685a4d0b443bb3d2502ef4008ba7d8f9430c1
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-10-29 10:34:12 +0300

    implemenation of user api-routes

---
+++ src/router/user.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as userController from '../controllers/users.Controllers'
+const userRouter=Router()
+
+userRouter.get("/users",userController.getUsers)
+userRouter.get("/users/admins",userController.getAdmins)
+userRouter.get("/users/admin/:admin_id",userController.getAdminById)
+userRouter.get("/users/admin/",userController.getUserByEmail)
+userRouter.get("/users/members",userController.getMembers)
+userRouter.get("/users/member/:member_id",userController.getMemberById)
+userRouter.post("/users/create/",userController.createUser)
+userRouter.delete("/users/delete/:id",userController.deleteUser)
+
+
+export default userRouter
